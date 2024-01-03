3e35d3f40c629876733f24b5a2699b308e9a6272
Yuliia Ataman <47396774+chandrashka@users.noreply.github.com>
2024-01-03 12:26:25 +0100

    Add files via upload

--- query.sql
+++ query.sql
@@ -1,18 +1,25 @@
--- Query 1: Retrieve track information and artist name for tracks in Spotify playlists
-SELECT Track.track_name, Artist.artist_name
-FROM Track
-JOIN Playlist ON Track.track_id = Playlist.track_id
-JOIN Artist ON Track.artist_id = Artist.artist_id
-WHERE Playlist.in_spotify_playlists > 0;
-
--- Query 2: Retrieve the total streams for each track
-SELECT Track.track_name, SUM(Streams.streams) AS total_streams
+-- Query 2a: Sort streams
+SELECT Track.track_name, Streams.streams
 FROM Track
 JOIN Streams ON Track.track_id = Streams.track_id
-GROUP BY Track.track_name;
+ORDER BY Streams.streams DESC
 
--- Query 3: Retrieve tracks that are in both Apple and Deezer charts
-SELECT Track.track_name
+-- Query 2b: Percentage of tracks in playlists that are higher than 50 place
+SELECT COUNT(Playlist.track_id) AS playlist_count, 'Spotify' AS platform
+FROM Playlist
+WHERE Playlist.in_spotify_playlists > 50
+UNION
+SELECT COUNT(Playlist.track_id) AS playlist_count, 'Apple' AS platform
+FROM Playlist
+WHERE Playlist.in_apple_playlists > 50
+UNION
+SELECT COUNT(Playlist.track_id) AS playlist_count, 'Deezer' AS platform
+FROM Playlist
+WHERE Playlist.in_deezer_playlists > 50;
+
+-- Query 2c: Number of tracks in Spotify charts based on energy percentage that are higher than 100 place
+SELECT Track.energy_percent, COUNT(Charts.track_id) AS track_count
 FROM Track
 JOIN Charts ON Track.track_id = Charts.track_id
-WHERE Charts.in_apple_charts > 0 AND Charts.in_deezer_charts > 0;
+WHERE Charts.in_spotify_charts < 100
+GROUP BY Track.energy_percent;
